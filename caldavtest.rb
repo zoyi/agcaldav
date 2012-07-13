2fdc34ea7432741dfae3d23463672f814e03dd88
Martin Povolny <martin.povolny@solnet.cz>
2012-07-13 16:08:46 +0200

    add test for more caldav methods

--- caldavtest.rb
+++ caldavtest.rb
@@ -1,22 +1,73 @@
 require './lib/caldav.rb'
 
-myevent = Event.new
-myevent.dtstart = DateTime.parse( '2011/07/07 09:45')
-myevent.dtend = DateTime.parse( '2011/07/07 10:45')
-myevent.summary = "Yo Yo YO"
+class CaldavTester
+
+    def initialize
+        $caldav = {
+            :host     => 'mail.server.com',
+            :port     => 443,
+            :url      => '/caldav.php/martin.povolny@solnet.cz/test',
+            :user     => 'user@test.com',
+            :password => 'yourpassword'
+        }
+        begin
+            require '~/.caldav_cred.rb'
+        rescue LoadError
+        end
+        
+        @cal = Caldav.new $caldav[:host], $caldav[:port], $caldav[:url], $caldav[:user], $caldav[:password]
+    end
+
+    attr :cal
+
+    def test_create_event
+        myevent = Event.new
+        myevent.dtstart = DateTime.parse( '2012/08/08 09:45')
+        myevent.dtend = DateTime.parse( '2012/08/08 10:45')
+        myevent.summary = "Yo Yo YO"
+        
+        
+        uuid, response = cal.create myevent
+        
+        puts "CREATE Response #{response.code} #{response.message}: #{response.body}"
+        puts "UUID: #{uuid}"
+
+        return uuid
+    end
+
+    def test_delete_event( uuid )
+        response = cal.delete uuid
+        puts "DETELE Response #{response.code} #{response.message}: #{response.body}"
+    end
+
+    def test_get_event( uuid )
+        ev, response = cal.get uuid
+        puts "GET Response #{response.code} #{response.message}: #{response.body}"
+        puts ev
+    end
+
+    def test_read_toto
+        res = cal.todo
+        
+        res.each{ |todo| 
+            p todo
+        }
+    end
+    
+    def test_report
+        p cal.report "20111201T000000", "20131231T000000"
+    end
+end
 
 
+t = CaldavTester.new
 
-host = 'mail1.lite3d.com'
-port = 443
-url = '/dav/cmatthews@lite3d.com/Time'
-user = 'cmatthews'
-password = 'password'
+uuid = t.test_create_event
+t.test_get_event( uuid )
+t.test_delete_event( uuid )
 
+t.test_report
 
-cal = Caldav.new host, port, url, user, password 
+#t.test_read_toto
 
-uuid, response = cal.create myevent
-
-  puts "Response #{response.code} #{response.message}: #{response.body}"
 
