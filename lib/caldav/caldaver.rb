4c0aabdd0b239fb4e862e506c085ccf2e2e5ea6a
Martin Povolny <martin.povolny@solnet.cz>
2012-08-28 10:52:15 +0200

    some work on client

--- lib/caldav/caldaver.rb
+++ lib/caldav/caldaver.rb
@@ -6,43 +6,91 @@ require 'optparse'
 module CalDAV
 
 class CalDAVer
-    def run_args( args )
-        o = nil
 
-        help = Proc.new do
-            puts o.help
-            exit 1
+    def create_object( options )
+        if options[:raw]
+            # STDIN
+            return STDIN.read(nil)
+        else
+            # options[:subject]
+            # options[:login]
+            # options[:summary]
+            # options[:begin]
+            # options[:end]
+            # options[:due]
+            case options[:what].intern
+            when :task
+                # FIXME
+            when :event
+                # FIXME
+            when :contact
+                # FIXME
+            else
+                print_help_and_exit if options[:command].to_s.empty? or options[:uri].to_s.empty?
+            end
         end
+    end
 
+    def print_help_and_exit
+        puts @o.help
+        exit 1
+    end
+
+    def run_args( args )
         options = {}
-        o = OptionParser.new do |o|
+        @o = OptionParser.new do |o|
             o.on('-p', '--password',      String, 'Password')     { |p|   options[:password] = p }
             o.on('-u', '--user',          String, 'User (login)') { |l|   options[:login]    = l }
             o.on('--uri [STRING]',        String, 'Calendar URI') { |uri| options[:uri]      = uri }
-            o.on('--format [STRING]',     String, 'Format of output') { |fmt| options[:fmt]  = fmt }
+            o.on('--format [STRING]',     String, 'Format of output: raw,pretty,[debug]') { 
+                                                                    |fmt| options[:fmt]  = fmt }
             o.on('--command [STRING]',    String, 'Command')      { |c|   options[:command]  = command }
-            o.on('--begin [DATETIME]',    String, 'Start time') { |dt|  options[:begin]    = dt }
-            o.on('--end [DATETIME]',      String, 'End time')   { |dt|  options[:end]      = dt }
+            # what to create
+            o.on('--what [STRING]',       String, 'Event/task/contact') { |c| options[:command]  = command }
+            o.on('--raw',                         'Read raw data (event/task/contact) from STDIN') { |raw|   options[:raw] = true }
+            # report and event options
+            o.on('--begin [DATETIME]',    String, 'Start time')   { |dt|  options[:begin]    = dt }
+            o.on('--end [DATETIME]',      String, 'End time')     { |dt|  options[:end]      = dt }
+            o.on('--due [DATETIME]',      String, 'Due time')     { |dt|  options[:due]      = dt }
             #o.on('--begin [DATETIME]',    DateTime, 'Start time') { |dt|  options[:begin]    = dt }
             #o.on('--end [DATETIME]',      DateTime, 'End time')   { |dt|  options[:end]      = dt }
-            o.on('-h') { help.call }
+            # event options
+            o.on('--summary  [string]',   String, 'summary of event/task')  { |s|  options[:summary]  = s }
+            o.on('--location [string]',   String, 'location of event/task') { |s|  options[:location] = s }
+            o.on('--subject  [string]',   String, 'subject of event/task')  { |s|  options[:subject]  = s }
+            o.on('-h') { print_help_and_exit }
         end
 
-        o.parse( args )
+        @o.parse( args )
 
-        help.call if options[:command].to_s.empty? or options[:uri].to_s.empty?
+        print_help_and_exit if options[:command].to_s.empty? or options[:uri].to_s.empty?
 
         cal = cal.new( options[:uri], options[:login], options[:password] )
 
-        case options[:command].to_s
-        when 'create'
-        when 'delete'
-        #when 'modify'
-        when 'get'
-        when 'report'
+        if options[:format]
+            cal.format = case options[:format].intern
+                when :raw
+                    CalDAV::Format::Raw.new
+                when :pretty
+                    CalDAV::Format::Pretty.new
+                when :debug
+                    CalDAV::Format::Debug.new
+                else
+                    nil
+            end
+        end
+
+        case options[:command].intern
+        when :create
+            obj = create_object( options )
+        when :delete
+        when :modify
+            obj = create_object( options )
+        when :get
+        when :report
             cal.report( options[:begin], options[:end] )
         else
-            help.call
+            print_help_and_exit
         end
     end
 end
